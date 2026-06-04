5614ea0db81c17db11c1521371db6ef4be6efcdc
jon <jonathan.thermidor@outlook.com>
2026-06-05 00:32:42 +0200

    Adding data to the app.

--- patient/src/main/java/com/medilabo/patient/PatientApplication.java
+++ patient/src/main/java/com/medilabo/patient/PatientApplication.java
@@ -1,39 +1,35 @@
 package com.medilabo.patient;
 
+import com.medilabo.patient.model.Patient;
+import com.medilabo.patient.repository.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class PatientApplication {
+import java.util.List;
 
-//	@Autowired
-//    PatientRepository patientRepository;
+@SpringBootApplication
+public class PatientApplication implements CommandLineRunner {
+
+	@Autowired
+    PatientRepository patientRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(PatientApplication.class, args);
 	}
 
-//	@Override
-//	public void run(String... args) throws Exception {
-//
-//		List<Patient> patients = List.of(
-//				new Patient(null, "Test", "TestNone", LocalDate.of(1966,12,31), "F","1 Brookside St", "100-222-3333"),
-//				new Patient(null, "Test", "TestBorderline", LocalDate.of(1945,6,24), "M","2 High St", "200-333-4444"),
-//				new Patient(null, "Test", "TestInDanger", LocalDate.of(2004,6,18), "M","3 Club Road", "300-444-5555"),
-//				new Patient(null, "Test", "TestEarlyOnset", LocalDate.of(2002,6,28), "F","4 Valley Dr", "400-555-6666")
-//		);
-//		patientRepository.saveAll(patients);
-//	}
-//
-//	@Override
-//	public void run(String... args) throws Exception {
-//
-//		List<Patient> patients = List.of(
-//				new Patient(null, "Test", "TestNone", "1966-12-31", "F", "1 Brookside St", "100-222-3333"),
-//				new Patient(null, "Test", "TestBorderline", "1945-06-24", "M", "2 High St", "200-333-4444"),
-//				new Patient(null, "Test", "TestInDanger", "2004-06-18", "M", "3 Club Road", "300-444-5555"),
-//				new Patient(null, "Test", "TestEarlyOnset", "2002-06-28", "F", "4 Valley Dr", "400-555-6666")
-//		);
-//		patientRepository.saveAll(patients);
-//	}
+	@Override
+	public void run(String... args) {
+
+		if (patientRepository.count() == 0) {
+			List<Patient> patients = List.of(
+					new Patient(null, "Test", "TestNone", "1966-12-31", "F", "1 Brookside St", "100-222-3333"),
+					new Patient(null, "Test", "TestBorderline", "1945-06-24", "M", "2 High St", "200-333-4444"),
+					new Patient(null, "Test", "TestInDanger", "2004-06-18", "M", "3 Club Road", "300-444-5555"),
+					new Patient(null, "Test", "TestEarlyOnset", "2002-06-28", "F", "4 Valley Dr", "400-555-6666")
+			);
+			patientRepository.saveAll(patients);
+		}
+	}
 }
